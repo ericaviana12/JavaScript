b9337fbcb409beefaba60c7aac2d5084b1442864
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-09-15 13:01:53 -0300

    Atualizar o Variaveis.js

--- Fundamentos/Variaveis.js
+++ Fundamentos/Variaveis.js
@@ -14,7 +14,7 @@ nome = "Erica Viana" //quando escreve o nome dentro de aspas, ele cria a variáv
 console.log(typeof(nome)) //atribui à variável "nome" um valor (typeof é para identificar qual o tipo de variável (String))
 console.log(nome) //para escrever o conteúdo dessa variável configurada
 
-idade = 56 //quando escreve a idade sem aspas, ele cria a variável Number
+idade = 31 //quando escreve a idade sem aspas, ele cria a variável Number
 console.log(typeof(idade)) //atribui à variável "idade" um valor (typeof é para identificar qual o tipo de variável (Number))
 console.log(idade) //para escrever o conteúdo dessa variável configurada
 

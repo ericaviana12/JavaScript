bef9c38729f40d090b7117d465a185c95a32c4e1
Erica Viana <erica.viana.soares@gmail.com>
2024-09-24 19:41:11 -0300

    Atualização

---
+++ Exercicios/Academia.js
@@ -0,0 +1,27 @@
+/**
+ * *Ficha de um aluno da cademia
+ * @author Erica Viana
+ */
+
+//variaveis
+let nome  
+let idade
+let peso
+let altura
+let vip 
+
+console.clear()
+nome = "Erica Viana"
+idade = 31
+peso = 58
+altura = 1.57
+vip = true
+
+//saida
+console.log("Ficha do aluno")
+console.log("_____________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)

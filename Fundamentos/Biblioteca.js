60d7e1019db155517335cad734d15a1976ec1efc
Erica Viana <erica.viana.soares@gmail.com>
2024-09-26 22:19:21 -0300

    Atualização

---
+++ Fundamentos/Biblioteca.js
@@ -0,0 +1,11 @@
+/**
+ * Exemplo de uso de pacotes externos ("bibliotecas")
+ * @author Erica Viana
+ */
+
+// Importar o pacote
+const colors = require('colors')
+
+console.clear()
+
+console.log("Hello World".blue.underline.italic)

b371c1696b316e3e91e0ddffc8523870855b7fce
Erica Viana <erica.viana.soares@gmail.com>
2024-10-01 22:14:01 -0300

    Atualização

--- Exercicios/Porcentagem.js
+++ Exercicios/Porcentagem.js
@@ -13,6 +13,7 @@ console.clear()
 
 // Exibir o texto entre "" (aspas)
 console.log("Cálculo de porcentagem de um valor")
+console.log("x% de y = valor")
 
 // Entrada - Atribuir valores as variáveis
 x = Number(input.question("Digite o valor de x: "))
@@ -22,4 +23,4 @@ y = Number(input.question("Digite o valor de y: "))
 valor = (x * y)/100
 
 // Saída - Resultado do processamento
-console.log(`Valor: ${valor.toFixed(2)}`)
+console.log(`${x}% de ${y} = ${valor.toFixed(2)}`)

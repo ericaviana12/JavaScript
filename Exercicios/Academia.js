faa4a0307b10efa14da53e5272076f36ec6ba795
Erica Viana <erica.viana.soares@gmail.com>
2024-10-15 22:19:28 -0300

    Atualização

--- Exercicios/Academia.js
+++ Exercicios/Academia.js
@@ -40,3 +40,17 @@ console.log(`IMC ${imc.toFixed(2)}`)
 // Processamento - "IMC = peso (altura * altura)"
 // Saída - IMC
 // .toFixed(2) - Limita os caracteres numéricos às casas decimais que estão entre o ()
+
+if (imc < 18.5) {
+    console.log("Abaixo do peso")
+  } else if (imc < 25) {
+    console.log("Peso normal")
+  } else if (imc < 30) {
+    console.log("Sobrepeso")
+  } else if (imc < 35) {
+    console.log("Obesidade grau 1")
+  } else if (imc < 40) {
+    console.log("Obesidade grau 2")
+  } else {
+    console.log("Obesidade grau 3 (mórbida)")
+  }

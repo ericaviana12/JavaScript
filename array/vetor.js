0c1c6514b8d612aff66583170726297980288d9c
Erica Viana <erica.viana.soares@gmail.com>
2024-11-26 20:02:25 -0300

    Atualização

---
+++ array/vetor.js
@@ -0,0 +1,12 @@
+/**
+ * Estrutura de dados array
+ * @author Erica Viana
+ */
+
+//                 [0]      [1]      [2]      [3]      [4]
+let alunosEM1 = ["Tania", "Pedro", "Maria", "Joao", "Viviane"]
+
+console.clear()
+
+console.log(typeof(alunosEM1))
+console.log(alunosEM1)
